201936bfa08660aec94f584d91a45d7643930390
alex2345-code <aortizp1903@alumno.ipn.mx>
2021-10-03 22:47:20 -0500

    habia otro error aparte de el de los tokens, f

--- 01 Cesar/js/logica.js
+++ 01 Cesar/js/logica.js
@@ -1,17 +1,17 @@
-var cesar = cesar || (function(){
-    var proceso = function(txt, desp, action){
-        var replace = (function(){
+var cesar = cesar || (function () {
+    var proceso = function (txt, desp, action) {
+        var replace = (function () {
             //primero necesito tener la matriz del alfabeto
             //hay que recorrar que el cifrado lo hace caracter por caracter
             var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-                        'x', 'y', 'z'];
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+                'x', 'y', 'z'];
             var l = abc.length;
 
             //necesitamos obtener la posicion que va  a venir por parte 
             //de la llave privada
 
-            return function(c){
+            return function (c) {
                 //vamos a saber la posicion
                 var i = abc.indexOf(c.toLowerCase());
                 //necesitamos saber es donde estamos adentro de la matriz
@@ -20,20 +20,20 @@ var cesar = cesar || (function(){
                 //alert(c);
                 //alert(i);
 
-                if(i != -1){
+                if (i != -1) {
                     //primero obtenemos la posicion para el desp
                     var pos = i;
                     //que voy a hacer cifrar o descifrar
-                    if(action){
+                    if (action) {
                         //cifrar para adelante
                         pos += desp;
                         //como se va a mover
-                        pos -= (pos >= l)?l:0;
-                    }else{
+                        pos -= (pos >= l) ? l : 0;
+                    } else {
                         //descifrar para atras
                         pos -= desp;
                         //movimiento
-                        pos += (pos < 0)?l:0;
+                        pos += (pos < 0) ? l : 0;
                     }
                     return abc[pos];
 
@@ -44,18 +44,18 @@ var cesar = cesar || (function(){
         //tenemos que saber que el texto este acorde al abc
         var re = (/([a-z])/ig);
         //una funcion que se encargue del intercambio
-        return String(txt).replace(re, function(match){
+        return String(txt).replace(re, function (match) {
             return replace(match);
         });
-        
+
     };
 
-    return{
-        encode : function(txt, desp){
+    return {
+        encode: function (txt, desp) {
             return proceso(txt, desp, true);
         },
 
-        decode : function(txt, desp){
+        decode: function (txt, desp) {
             return proceso(txt, desp, false);
         }
     };
@@ -63,14 +63,14 @@ var cesar = cesar || (function(){
 
 //funcion de cifrado
 
-function cifrar(){
+function cifrar() {
     document.getElementById("resultado").innerHTML =
-    cesar.encode(document.getElementById("cadena").value, 3);
+        cesar.encode(document.getElementById("cadena").value, 3);
 }
 
 //funcion de descifrado
 
-function descifrar(){
+function descifrar() {
     document.getElementById("resultado").innerHTML =
-    cesar.decode(document.getElementById("cadena").value, 3);
+        cesar.decode(document.getElementById("cadena").value, 3);
 }
